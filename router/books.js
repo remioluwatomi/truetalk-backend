23435158737a5a8ffdb2786cc638786c10ce7165
tomi <remioluwatomicephas@gmail.com>
2025-02-14 01:32:35 +0100

    feat(booksRouter): updated the router for books endpoint
    included multer middleware and decodeResourceId util function

--- router/books.js
+++ router/books.js
@@ -5,11 +5,20 @@ import {
   uploadBook,
   deleteBook,
 } from "../controller/booksCtrl.js";
+import multer from "multer";
+import decodeResourceId from "../utils/decodeResourceId.js";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const booksRouter = Router();
 
-booksRouter.route("/").get(getAllBooks).post(uploadBook).put(updateBook);
+booksRouter
+  .route("/")
+  .get(getAllBooks)
+  .post(upload.fields([{ name: "book-pdf" }, { name: "img" }]), uploadBook)
+  .put(upload.fields([{ name: "book-pdf" }, { name: "img" }]), updateBook);
 
-booksRouter.delete("/:id", deleteBook);
+booksRouter.delete("/:id", decodeResourceId, deleteBook);
 
 export default booksRouter;
