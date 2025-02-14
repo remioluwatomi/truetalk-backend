427e726181567b284b589f3cd4e0f3688ba8b05a
tomi <remioluwatomicephas@gmail.com>
2025-02-14 01:51:30 +0100

    feat(galleryRouter): updated the router for gallery endpoint
    included multer middleware and decodeResourceId util function

--- router/gallery.js
+++ router/gallery.js
@@ -4,11 +4,18 @@ import {
   uploadImage,
   deleteImage,
 } from "../controller/galleryCtrl.js";
+import multer from "multer";
+import decodeResourceId from "../utils/decodeResourceId.js";
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const galleryRouter = Router();
 
-galleryRouter.route("/").get(getAllImages).post(uploadImage);
+galleryRouter
+  .route("/")
+  .get(getAllImages)
+  .post(upload.single("img"), uploadImage);
 
-galleryRouter.delete("/:id", deleteImage);
+galleryRouter.delete("/:id", decodeResourceId, deleteImage);
 
 export default galleryRouter;
