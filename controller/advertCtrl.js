665fbe1dd0021f3e5cb1356c42681851080873d7
tomi <remioluwatomicephas@gmail.com>
2025-02-21 23:49:31 +0100

    feat(controllers): implemented the uploadAdverts controller

--- controller/advertCtrl.js
+++ controller/advertCtrl.js
@@ -12,11 +12,31 @@ export const getAdverts = async (req, res) => {
   }
 };
 
-export const uploadAdvert = (_, res) => {
-  return res.json({
-    route: "Advert",
-    method: "POST",
-  });
+export const uploadAdvert = (req, res, next) => {
+  const { img, title, description, advertLink } = req.resourceInfo;
+
+  try {
+    Advert.create({
+      title,
+      description,
+      advertLink,
+      img,
+    })
+      .then((newAdvert) => {
+        res.status(201).json({
+          message: "Advert uploaded successfully",
+          uploaded: true,
+        });
+      })
+      .catch((e) => {
+        res.status(400).json({
+          uploaded: false,
+          message: "Advert upload failed. Advert title already exist",
+        });
+      });
+  } catch (e) {
+    next(e);
+  }
 };
 
 export const modifyAdvert = async (req, res, next) => {
