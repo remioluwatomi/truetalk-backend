9d58ab075b2e64740761027be5166bc215a67ffc
tomi <remioluwatomicephas@gmail.com>
2025-02-22 00:02:27 +0100

    feat(controllers): implemented the modifyAdverts and deleteAdverts controller

--- controller/advertCtrl.js
+++ controller/advertCtrl.js
@@ -40,15 +40,62 @@ export const uploadAdvert = (req, res, next) => {
 };
 
 export const modifyAdvert = async (req, res, next) => {
-  return res.json({
-    route: "Advert",
-    method: "PUT",
-  });
+  const { img, title, description, advertLink, keyTitle } = req.resourceInfo;
+
+  try {
+    const existingAdvert = await Advert.findOne({
+      where: {
+        title: keyTitle,
+      },
+    });
+
+    if (!existingAdvert) {
+      return res.status(404).json({
+        message:
+          "Advert was not uploaded successfully because the title exist. You can only update an already existed advert",
+        updated: false,
+      });
+    }
+
+    const updateObject = {
+      title,
+      description,
+      advertLink,
+    };
+    if (img) {
+      updateObject.img = img;
+    }
+
+    Advert.update(updateObject, {
+      where: {
+        title: keyTitle,
+      },
+    })
+      .then((advert) => {
+        res
+          .status(200)
+          .json({ message: "Advert updated successfully", updated: true });
+      })
+      .catch((e) => {
+        next(e);
+      });
+  } catch (e) {
+    next(e);
+  }
 };
 
 export const deleteAdvert = async (req, res, next) => {
-  return res.json({
-    route: "Advert",
-    method: "DELETE",
-  });
+  const { resourceId } = req;
+  try {
+    await Advert.destroy({
+      where: {
+        title: resourceId,
+      },
+    });
+    res.json({ resourceId, deleted: true });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.json({ resourceId, deleted: false });
+  }
 };
